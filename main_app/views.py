b6cc6d227c4a596b7f812dfcff41a0b054bd5974
Jared Watson <jjaredwatson@gmail.com>
2017-07-08 15:10:37 -0500

    connected model to view and ran migration

--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,6 @@
 from django.shortcuts import render
+from .models import Treasure
 
 def index(request):
-	return render(request, 'index.html', {'treasures': treasures})
-
-class Treasure:
-    def __init__(self, name, value, material, location):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-
-treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creed, NM"),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
-    Treasure('Coffee Can', 20.00, 'tin', "Acme, CA")
-]
+    treasures = Treasure.objects.all()
+    return render(request, 'index.html', {'treasures':treasures})
