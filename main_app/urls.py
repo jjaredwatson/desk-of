ade4c8cf155911e2539e22592d19e735b7b14dc6
Jared Watson <jjaredwatson@gmail.com>
2017-07-08 18:28:07 -0500

    added the ability to read

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from main_app import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^([0-9]+)/$', views.show, name = 'show')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,7 @@ from .models import Treasure
 def index(request):
     treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures':treasures})
+
+def show(request, treasure_id):
+	treasure = Treasure.objects.get(id=treasure_id)
+	return render(request, 'show.html', {'treasure': treasure}) 
