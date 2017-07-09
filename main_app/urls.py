f86c3119e57b836c267cb746ad72f9a11f8f257d
Jared Watson <jjaredwatson@gmail.com>
2017-07-09 12:42:30 -0500

    added ability to create

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from main_app import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^([0-9]+)/$', views.show, name = 'show')
+    url(r'^([0-9]+)/$', views.show, name = 'show'),
+    url(r'^post_url/$', views.post_treasure, name="post_treasure")
 ]
