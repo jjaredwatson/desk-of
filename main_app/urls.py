da761a2a917ceb5e70acdd04f334d1af0e047fbc
Jared Watson <jjaredwatson@gmail.com>
2017-07-10 15:33:13 -0500

    added joural url and style

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,9 +4,9 @@ from main_app import views
 urlpatterns = [
     url(r'^$', views.desk),
     url(r'^drinks/$', views.index_drink),
-    url(r'^([0-9]+)/$', views.show_drink, name = 'show'),
+    url(r'^drinks/([0-9]+)/$', views.show_drink, name = 'show_drink'),
     url(r'^drinks/post_url/$', views.post_drink, name="post_drink"),
     url(r'^journal/$', views.index_journal),
-    url(r'^([0-9]+)/$', views.show_journal, name = 'show'),
+    url(r'^journal/([0-9]+)/$', views.show_journal, name = 'show_journal'),
     url(r'^journal/post_url/$', views.post_journal, name="post_journal")
 ]
